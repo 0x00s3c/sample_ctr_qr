53afc9d6c2076046beb85db02ca6e17d75be0a72
mikefrancoq <mikefrancoyt@gmail.com>
2022-04-27 02:02:19 +0800

    Update~
    Updating datas~

---
+++ users/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Registration Form</title>
+</head>
+<body>
+	<center>
+		<form action="insert.php" method="post"><p>
+				<label>First Name:</label>
+				<input type="text" name="first_name" id="firstname">
+			    </p>
+
+                <p>
+				<label>Last Name:</label>
+				<input type="text" name="last_name" id="lastname">
+			    </p>
+
+                <p>
+				<label>Middle Name:</label>
+				<input type="text" name="last_name" id="middlename">
+			    </p>
+
+                <p>
+				<label>Email:</label>
+				<input type="text" name="gender" id="email">
+			    </p>
+
+                <p>
+				<label>Contact #</label>
+				<input type="text" name="address" id="contact">
+			    </p>
+
+                <p>
+				<label for="address">Address:</label>
+				<input type="text" name="email" id="emailAddress">
+			    </p>
+
+                <p>
+				<label for="city_id">City/State</label>
+				<input type="text" name="email" id="emailAddress">
+			    </p>
+
+                <p>
+				<label for="emailAddress">Email Address:</label>
+				<input type="text" name="email" id="emailAddress">
+			    </p>
+
+
+			
+			<input type="submit" value="Submit">
+		</form>
+	</center>
+</body>
+
+</html>
